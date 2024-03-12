2eafdc3880b0330c84b33b5a7ad2507dcc53da57
Frank <162546671+frankmtl@users.noreply.github.com>
2024-03-12 04:55:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#Day 1
+
+print("Hello Replit")
+print('How are you?')
+
+print('''
+Here's an exact
+      BIT
+       OF
+        TEXT
+''')
+
+print('What could go wrong?') #print works & Print won't: case sensitive
+
+print("Please work") #need to put quotes around the text
+
+print('''
+Frank
+March 11th, 2024
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do 
+this from my phone wherever I happen to be. No excuses 
+for not coding from the middle of a field!
+I am feeling 😃
+You can follow my progress at replit.com/@F
+''')
