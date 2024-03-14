4db36476cae58de5230fa7481427d86bbb36d7b1
Frank <162546671+frankmtl@users.noreply.github.com>
2024-03-14 02:43:44 +0000

    Day 9 completed

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
+#Day 9
+
+'''
+print('Generation Identifier')
+print('=====================')
+
+year = int(input('Which year were you born? '))
+if year >= 1883 and year <= 1900:
+  print('You are from the Lost Generation')
+elif year >= 1901 and year <= 1927:
+  print('You are from the Greatest Generation')
+elif year >= 1928 and year <= 1945:
+  print('You are from the Silent Generation')
+elif year >= 1946 and year <= 1964:
+  print('You are from the Baby Boomers')
+elif year >= 1965 and year <= 1980:
+  print('You are from Generation X')
+elif year >= 1981 and year <= 1996:
+  print('You are from the Millennials')
+elif year >= 1997 and year <= 2012:
+  print('You are from Generation Z')
+else:
+  print('You are from Generation Alpha')
+
+
+'''
+
+
+'''
+score = int (input("What was your score on the test? "))
+if score >= 80:
+  print("Not too shabby")
+elif score > 70:
+  print("Acceptable.")
+else:
+  print("Dude, you need to study more!")
+
+'''
+
+
+'''
+
+myPi = float(input("What is Pi to 3dp? "))
+if myPi == 3.142:
+  print('exactly!')  
+else:
+  print('try again 😭')
+
+'''
+
+
+'''
+
+myScore = int (input('Your score: '))
+if myScore > 100000:
+  print("Winner!")
+else:
+  print("Try again 😭")
+
+'''
+
+
+
+
+
 #Day 8
 
 '''
@@ -293,7 +358,6 @@ print("living in the amazing")
 print(myLocation)
 
 '''
-
 
 
 
